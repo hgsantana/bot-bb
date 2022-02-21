dc0520592eb67607ade7249007b16630b551bb99
Hugo Sant'Ana <contato@hs.tec.br>
2022-02-21 04:29:57 -0300

    fix: melhora fluxo do app

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { atualizaTudo } from './services/atualiza-dados'
 
 const app = express()
 
-atualizaTudo()
 
 app.all("*", routes)
 
 app.listen(4000, () => {
     console.log("Servidor na porta 4000")
+    atualizaTudo()
 })
