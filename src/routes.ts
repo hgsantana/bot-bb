59476b0b5b7dd63f1083bdff711324f6b11b2585
Hugo Sant'Ana <contato@hs.tec.br>
2022-02-22 01:50:59 -0300

    cut: remove migração de dados

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
 import { dadosAtualizados } from "./rotas/dados-atualizados";
 import { dadosCompletos } from "./rotas/dados-completos";
-import { migraDados } from "./rotas/migra-dados";
 
 export const routes = Router()
 
 routes.get("/dadosAtualizados/:tipo", dadosAtualizados)
 routes.get("/dadosCompletos/:tipo", dadosCompletos)
-routes.get("/migracao", migraDados)
