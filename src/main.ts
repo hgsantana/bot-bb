9a655de7da9efab30536597e60facc32e7378366
Hugo Sant'Ana <contato@hs.tec.br>
2022-02-27 03:53:52 -0300

    add: gzip

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import compression from 'compression'
 import express from 'express'
 import { routes } from './routes'
 import { iniciar } from './services/atualiza-dados'
 
 const app = express()
 
+app.use(compression())
 app.all("*", routes)
 
 const porta = process.env.PORT || 4000
