62d9b50f9df81a7f47fbc47d4168b80e88048355
Hugo Sant'Ana <contato@hs.tec.br>
2022-03-09 14:51:54 -0300

    add: array de websockets abertos

--- src/services/websocket-service.ts
+++ src/services/websocket-service.ts
@@ -1,5 +1,7 @@
 import { Server } from 'http';
-import ws, { WebSocketServer } from 'ws';
+import ws, { WebSocket, WebSocketServer } from 'ws';
+
+export const websocketsAbertos: WebSocket[] = []
 
 export const iniciarWebsocket = (server: Server) => {
     const websocketServer: WebSocketServer = new ws.Server({
@@ -12,6 +14,7 @@ export const iniciarWebsocket = (server: Server) => {
     server.on("upgrade", (request, socket, head) => {
         websocketServer.handleUpgrade(request, socket, head, (websocket) => {
             console.log("Nova conexão via websocket de", request.socket.remoteAddress)
+            websocketsAbertos.push(websocket)
 
             websocket.send('Iniciando contador...');
             let contador = 1
@@ -20,7 +23,11 @@ export const iniciarWebsocket = (server: Server) => {
                 contador++
             }, 1000)
 
-            websocket.on("close", () => console.log("Conexão de " + request.socket.remoteAddress + " encerrada."))
+            websocket.on("close", () => {
+                console.log("Conexão de " + request.socket.remoteAddress + " encerrada.")
+                const indiceWebsocket = websocketsAbertos.indexOf(websocket)
+                websocketsAbertos.splice(indiceWebsocket, 1)
+            })
         });
     });
 
