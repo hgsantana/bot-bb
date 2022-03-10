3da62b2a163f1b23c67d156c4cfce431501ccb85
Hugo Sant'Ana <contato@hs.tec.br>
2022-03-10 16:58:41 -0300

    add: mensagem de resposta

---
+++ src/models/bot-update-response.ts
@@ -0,0 +1,14 @@
+export type BotUpdateResponse = {
+    method: string
+    chat_id: number
+    text: string
+
+    parse_mode?: string
+    entities?: any[]
+    disable_web_page_preview?: boolean
+    disable_notification?: boolean
+    protect_content?: boolean
+    reply_to_message_id?: number
+    allow_sending_without_reply?: boolean
+    reply_markup?: any
+}
--- src/rotas/recebe-mensagens-bot.ts
+++ src/rotas/recebe-mensagens-bot.ts
@@ -1,14 +1,24 @@
 import { Request, Response } from 'express'
+import { respostaMOCK } from '../data/dados-mock'
 import { AMBIENTE } from '../main'
+import { BotUpdate } from '../models/bot-update'
+import { BotUpdateResponse } from '../models/bot-update-response'
 
 export const recebeMensagensBot = async (req: Request, res: Response) => {
-
+    const mensagem: BotUpdate = req.body
     if (!req.query
         || !req.query.token
         || req.query.token != AMBIENTE.TELEGRAM_TOKEN
     ) return res.status(401).send()
 
-    console.log("Mensagem recebida no bot:", req.body)
-    res.send()
+    console.log("Mensagem recebida no bot:", mensagem)
 
+    const resposta: BotUpdateResponse = {
+        chat_id: mensagem.message.chat.id,
+        method: "sendMessage",
+        parse_mode: "HTML",
+        text: `Olá, @${mensagem.message.from.username}. Segue atualização de status das convocações: <pre><code class="language-json">${respostaMOCK}</code></pre>`
+    }
+
+    res.setHeader("method", "POST").send()
 }
