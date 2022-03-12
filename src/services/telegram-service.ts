d2b11689b65442a3c2d3f90b858f2631990d24cc
Hugo Sant'Ana <contato@hs.tec.br>
2022-03-11 23:20:47 -0300

    fix: refatora código para melhor legibilidade e manutenção

--- src/services/telegram-service.ts
+++ src/services/telegram-service.ts
@@ -24,139 +24,147 @@ buscaDadosTelegram().then(dados => {
 })
 
 export const checaMensagem = (mensagemRecebida: BotUpdate): BotUpdateResponse | null => {
-
     if (!mensagemRecebida?.message?.text) return null
 
-    /********************* /iniciar *********************/
-    if (mensagemRecebida.message.text.toLocaleLowerCase().startsWith("/iniciar")) {
-        const chat = chatsCadastrados.find(c => c.id == mensagemRecebida.message.chat.id)
-        let text = ""
-        if (chat) text = `As atualizações já estão ativas para este chat.`
-        else {
-            console.log("Ativando atualizações para o chat:", mensagemRecebida.message.chat.id)
-            text = `Ativando atualizações para este chat. Para interrompê-las, use o comando /parar.`
-            chatsCadastrados.push({ id: mensagemRecebida.message.chat.id })
-            salvaDadosTelegram({ usuariosCadastrados, chatsCadastrados })
-        }
-        const reply_to_message_id = mensagemRecebida.message.message_id
-        return {
-            chat_id: mensagemRecebida.message.chat.id,
-            method: "sendMessage",
-            parse_mode: "HTML",
-            reply_to_message_id,
-            text
-        }
-    }
+    if (mensagemRecebida.message.text.toLocaleLowerCase().startsWith("/iniciar"))
+        return iniciar(mensagemRecebida)
 
-    /********************* /parar *********************/
-    if (mensagemRecebida.message.text.toLocaleLowerCase().startsWith("/parar")) {
-        const chat = chatsCadastrados.find(c => c.id == mensagemRecebida.message.chat.id)
-        let text = ""
-        if (chat) {
-            const indiceChat = chatsCadastrados.indexOf(chat)
-            chatsCadastrados.splice(indiceChat, 1)
-            salvaDadosTelegram({ usuariosCadastrados, chatsCadastrados })
-            console.log("Desativando atualizações para o chat:", mensagemRecebida.message.chat.id)
-            text = `As atualizações foram interrompidas para este chat.`
-        } else {
-            text = `Não há atualizações ativas para este chat. Caso deseje ativa-las, use o comando /iniciar.`
-        }
-        const reply_to_message_id = mensagemRecebida.message.message_id
-        return {
-            chat_id: mensagemRecebida.message.chat.id,
-            method: "sendMessage",
-            parse_mode: "HTML",
-            reply_to_message_id,
-            text
-        }
-    }
+    if (mensagemRecebida.message.text.toLocaleLowerCase().startsWith("/parar"))
+        return parar(mensagemRecebida)
 
-    /********************* /status *********************/
-    if (mensagemRecebida.message.text.toLocaleLowerCase().startsWith("/status")) {
-        const statusCompleto = geraStatusCompleto()
-        const reply_to_message_id = mensagemRecebida.message.message_id
-        return {
-            chat_id: mensagemRecebida.message.chat.id,
-            method: "sendMessage",
-            parse_mode: "HTML",
-            reply_to_message_id,
-            text: `Status atual das convocações:\n` +
-                `<pre>\n` +
-                `Não Convocados: ${statusCompleto.naoConvocados}\n` +
-                `Convocados: ${statusCompleto.convocados}\n` +
-                `\n` +
-                `Autorizadas: ${statusCompleto.autorizadas}\n` +
-                `Expedidas: ${statusCompleto.expedidas}\n` +
-                `Qualificação: ${statusCompleto.emQualificacao}\n` +
-                `Qualificados: ${statusCompleto.qualificados}\n` +
-                `Empossados: ${statusCompleto.empossados}\n` +
-                `Cancelados: ${statusCompleto.cancelados}\n` +
-                `Desistentes: ${statusCompleto.desistentes}\n` +
-                `Inaptos: ${statusCompleto.inaptos}\n` +
-                `\n` +
-                `${statusCompleto.ultimaAtualizacao.toLocaleString("pt-br", { timeStyle: 'short', dateStyle: 'short', timeZone: "America/Sao_Paulo" } as any)}\n` +
-                `</pre>`
-        }
-    }
+    if (mensagemRecebida.message.text.toLocaleLowerCase().startsWith("/status"))
+        return status(mensagemRecebida)
 
-    /********************* /cadastrar *********************/
-    if (mensagemRecebida.message.text.toLocaleLowerCase().startsWith("/cadastrar")) {
-        const nome = mensagemRecebida.message.text.split("/cadastrar")[1].replace(/\ \ /gi, " ").trim().toUpperCase()
-        const idDestinatario = `${mensagemRecebida.message.from.id}`
-        const usuario = usuariosCadastrados.find(u => u.id == idDestinatario)
-        const reply_to_message_id = mensagemRecebida.message.message_id
+    if (mensagemRecebida.message.text.toLocaleLowerCase().startsWith("/cadastrar"))
+        return cadastrar(mensagemRecebida)
 
-        let text = ``
-        const candidato = nomesCandidatos.find(n => n == nome)
-
-        if (!nome) text = `Você precisa usar a sintaxe correta: <pre>/cadastrar NOME COMPLETO</pre>`
-        else if (!candidato) text = `Este nome não existe no resultado final oficial.`
-        else {
-            text = `Olá, <a href="tg://user?id=${mensagemRecebida.message.from.id}">@${mensagemRecebida.message.from.first_name}</a>. ` +
-                `A partir de agora, você receberá os avisos de alterações para "${nome}" no privado. ` +
-                `Para cancelar os avisos, use o comando /descadastrar.`
-            if (usuario) usuario.nomeChecagem = nome
-            else {
-                console.log("Cadastrando novo usuário para envio de mensagens:", { id: idDestinatario, nomeChecagem: nome })
-                usuariosCadastrados.push({ id: idDestinatario, nomeChecagem: nome })
-                salvaDadosTelegram({ usuariosCadastrados, chatsCadastrados })
-            }
-        }
-
-
-        return {
-            chat_id: mensagemRecebida.message.chat.id,
-            method: "sendMessage",
-            parse_mode: "HTML",
-            reply_to_message_id,
-            text,
-        }
-    }
-
-    /********************* /descadastrar *********************/
-    if (mensagemRecebida.message.text.toLocaleLowerCase().startsWith("/descadastrar")) {
-        const idDestinatario = `${mensagemRecebida.message.from.id}`
-        const usuario = usuariosCadastrados.find(u => u.id == idDestinatario)
-
-        const reply_to_message_id = mensagemRecebida.message.message_id
-
-        let text = `A partir de agora, você não receberá mais avisos.`
-        if (usuario) {
-            console.log("Descadastrando usuário para envio de mensagens:", usuario)
-            usuariosCadastrados.splice(usuariosCadastrados.indexOf(usuario), 1)
-            salvaDadosTelegram({ usuariosCadastrados, chatsCadastrados })
-        } else text = `Você ainda não está cadastrado para receber avisos.`
-
-        return {
-            chat_id: mensagemRecebida.message.chat.id,
-            method: "sendMessage",
-            parse_mode: "HTML",
-            reply_to_message_id,
-            text,
-        }
-    }
+    if (mensagemRecebida.message.text.toLocaleLowerCase().startsWith("/descadastrar"))
+        return descadastrar(mensagemRecebida)
 
     return null
+}
+
+const iniciar = (mensagemRecebida: BotUpdate): BotUpdateResponse | null => {
+    const chat = chatsCadastrados.find(c => c.id == mensagemRecebida.message.chat.id)
+    let text = ""
+    if (chat) text = `As atualizações já estão ativas para este chat.`
+    else {
+        console.log("Ativando atualizações para o chat:", mensagemRecebida.message.chat.id)
+        text = `Ativando atualizações para este chat. Para interrompê-las, use o comando /parar.`
+        chatsCadastrados.push({ id: mensagemRecebida.message.chat.id })
+        salvaDadosTelegram({ usuariosCadastrados, chatsCadastrados })
+    }
+    const reply_to_message_id = mensagemRecebida.message.message_id
+    return {
+        chat_id: mensagemRecebida.message.chat.id,
+        method: "sendMessage",
+        parse_mode: "HTML",
+        reply_to_message_id,
+        text
+    }
+}
+
+const parar = (mensagemRecebida: BotUpdate): BotUpdateResponse | null => {
+    const chat = chatsCadastrados.find(c => c.id == mensagemRecebida.message.chat.id)
+    let text = ""
+    if (chat) {
+        const indiceChat = chatsCadastrados.indexOf(chat)
+        chatsCadastrados.splice(indiceChat, 1)
+        salvaDadosTelegram({ usuariosCadastrados, chatsCadastrados })
+        console.log("Desativando atualizações para o chat:", mensagemRecebida.message.chat.id)
+        text = `As atualizações foram interrompidas para este chat.`
+    } else {
+        text = `Não há atualizações ativas para este chat. Caso deseje ativa-las, use o comando /iniciar.`
+    }
+    const reply_to_message_id = mensagemRecebida.message.message_id
+    return {
+        chat_id: mensagemRecebida.message.chat.id,
+        method: "sendMessage",
+        parse_mode: "HTML",
+        reply_to_message_id,
+        text
+    }
+}
+
+const status = (mensagemRecebida: BotUpdate): BotUpdateResponse | null => {
+    const statusCompleto = geraStatusCompleto()
+    const reply_to_message_id = mensagemRecebida.message.message_id
+    return {
+        chat_id: mensagemRecebida.message.chat.id,
+        method: "sendMessage",
+        parse_mode: "HTML",
+        reply_to_message_id,
+        text: `Status atual das convocações:\n` +
+            `<pre>\n` +
+            `Não Convocados: ${statusCompleto.naoConvocados}\n` +
+            `Convocados: ${statusCompleto.convocados}\n` +
+            `\n` +
+            `Autorizadas: ${statusCompleto.autorizadas}\n` +
+            `Expedidas: ${statusCompleto.expedidas}\n` +
+            `Qualificação: ${statusCompleto.emQualificacao}\n` +
+            `Qualificados: ${statusCompleto.qualificados}\n` +
+            `Empossados: ${statusCompleto.empossados}\n` +
+            `Cancelados: ${statusCompleto.cancelados}\n` +
+            `Desistentes: ${statusCompleto.desistentes}\n` +
+            `Inaptos: ${statusCompleto.inaptos}\n` +
+            `\n` +
+            `${statusCompleto.ultimaAtualizacao.toLocaleString("pt-br", { timeStyle: 'short', dateStyle: 'short', timeZone: "America/Sao_Paulo" } as any)}\n` +
+            `</pre>`
+    }
+}
+
+const cadastrar = (mensagemRecebida: BotUpdate): BotUpdateResponse | null => {
+    const nome = mensagemRecebida.message.text.split("/cadastrar")[1].replace(/\ \ /gi, " ").trim().toUpperCase()
+    const idDestinatario = `${mensagemRecebida.message.from.id}`
+    const usuario = usuariosCadastrados.find(u => u.id == idDestinatario)
+    const reply_to_message_id = mensagemRecebida.message.message_id
+
+    let text = ``
+    const candidato = nomesCandidatos.find(n => n == nome)
+
+    if (!nome) text = `Você precisa usar a sintaxe correta: <pre>/cadastrar NOME COMPLETO</pre>`
+    else if (!candidato) text = `Este nome não existe no resultado final oficial.`
+    else {
+        text = `Olá, <a href="tg://user?id=${mensagemRecebida.message.from.id}">@${mensagemRecebida.message.from.first_name}</a>. ` +
+            `A partir de agora, você receberá os avisos de alterações para "${nome}" no privado. ` +
+            `Para cancelar os avisos, use o comando /descadastrar.`
+        if (usuario) usuario.nomeChecagem = nome
+        else {
+            console.log("Cadastrando novo usuário para envio de mensagens:", { id: idDestinatario, nomeChecagem: nome })
+            usuariosCadastrados.push({ id: idDestinatario, nomeChecagem: nome })
+            salvaDadosTelegram({ usuariosCadastrados, chatsCadastrados })
+        }
+    }
+
+    return {
+        chat_id: mensagemRecebida.message.chat.id,
+        method: "sendMessage",
+        parse_mode: "HTML",
+        reply_to_message_id,
+        text,
+    }
+}
+
+const descadastrar = (mensagemRecebida: BotUpdate): BotUpdateResponse | null => {
+    const idDestinatario = `${mensagemRecebida.message.from.id}`
+    const usuario = usuariosCadastrados.find(u => u.id == idDestinatario)
+
+    const reply_to_message_id = mensagemRecebida.message.message_id
+
+    let text = `A partir de agora, você não receberá mais avisos.`
+    if (usuario) {
+        console.log("Descadastrando usuário para envio de mensagens:", usuario)
+        usuariosCadastrados.splice(usuariosCadastrados.indexOf(usuario), 1)
+        salvaDadosTelegram({ usuariosCadastrados, chatsCadastrados })
+    } else text = `Você ainda não está cadastrado para receber avisos.`
+
+    return {
+        chat_id: mensagemRecebida.message.chat.id,
+        method: "sendMessage",
+        parse_mode: "HTML",
+        reply_to_message_id,
+        text,
+    }
 }
 
 export const enviaMensagemPrivada = async (UsuarioRegistrado: UsuarioCadastrado, situacaoAnterior: string, candidato: Candidato) => {
