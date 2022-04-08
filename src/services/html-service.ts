0cd8dbf32362e5b7ae61914d4f6d9886d62bface
Hugo Sant'Ana <hs.guimaraes@outlook.com>
2022-04-07 23:39:47 -0300

    fix: altera para 300ms

--- src/services/html-service.ts
+++ src/services/html-service.ts
@@ -69,7 +69,7 @@ const atualizaTudo = async () => {
 const atualizaSituacao = async (
     candidatos: Candidato[],
     tipo: "TI" | "COMERCIAL",
-    msIntervalo = 100,
+    msIntervalo = 300,
     houveAlteracao = false
 ) => {
     candidatos = candidatos.filter(c => c.situacao != "Empossado" && c.situacao != "Desistente")
